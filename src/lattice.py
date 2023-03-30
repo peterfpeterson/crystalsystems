6e90860c1e9c5c31f85cffe9ddc54086809c6928
Pete Peterson <petersonpf@ornl.gov>
2023-03-30 14:15:37 -0400

    Changed to a director/builder to clean out constructor

--- src/lattice.py
+++ src/lattice.py
@@ -2,38 +2,11 @@ import numpy as np
 
 
 class Lattice:
-    def __init__(self, a=0.0, b=0.0, c=0.0, alpha=0.0, beta=0.0, gamma=0.0, a_vec=None, b_vec=None, c_vec=None):
-        if Lattice.__scalars_are_valid(a, b, c, alpha, beta, gamma):
-            self.set_scalars(a, b, c, alpha, beta, gamma)
-        elif Lattice.__vectors_are_valid(a_vec, b_vec, c_vec):
+    def __init__(self, a_vec, b_vec, c_vec):
+        if Lattice.__vectors_are_valid(a_vec, b_vec, c_vec):
             self.set_vectors(a_vec, b_vec, c_vec)
         else:
             raise RuntimeError("Do not know how to initialize Lattice from supplied parameters")
-
-    def __scalars_are_valid(a, b, c, alpha, beta, gamma) -> bool:
-        return a > 0 and b > 0 and c > 0 and alpha > 0 and beta > 0 and gamma > 0
-
-    def set_scalars(self, a, b, c, alpha, beta, gamma):
-        if not Lattice.__scalars_are_valid(a, b, c, alpha, beta, gamma):
-            raise ValueError("Scalar values are invalid")
-        cos_alpha = np.cos(np.deg2rad(alpha))
-        cos_beta = np.cos(np.deg2rad(beta))
-        cos_gamma = np.cos(np.deg2rad(gamma))
-        v_term = np.sqrt(
-            1
-            - cos_alpha * cos_alpha
-            - cos_beta * cos_beta
-            - cos_gamma * cos_gamma
-            + 2 * cos_alpha * cos_beta * cos_gamma
-        )
-
-        sin_gamma = np.sin(np.deg2rad(gamma))
-
-        self.a_vec = np.asarray([a, 0, 0])
-        self.b_vec = np.asarray([b * cos_gamma, b * sin_gamma, 0])
-        self.c_vec = np.asarray(
-            [c * cos_beta, c * (cos_alpha - cos_gamma * cos_beta) / sin_gamma, c * v_term / sin_gamma]
-        )
 
     def __vectors_are_valid(a_vec, b_vec, c_vec) -> bool:
         def is_ok(vector) -> bool:
@@ -70,3 +43,34 @@ class Lattice:
         np.testing.assert_allclose(self.a_vec, other.a_vec, atol=atol)
         np.testing.assert_allclose(self.b_vec, other.b_vec, atol=atol)
         np.testing.assert_allclose(self.c_vec, other.c_vec, atol=atol)
+
+
+class LatticeBuilder:
+    def constructFromScalars(a: float, b: float, c: float, alpha: float, beta: float, gamma: float) -> Lattice:
+        if not LatticeBuilder.__scalars_are_valid(a, b, c, alpha, beta, gamma):
+            raise RuntimeError("scalar values are invalid")
+
+        cos_alpha = np.cos(np.deg2rad(alpha))
+        cos_beta = np.cos(np.deg2rad(beta))
+        cos_gamma = np.cos(np.deg2rad(gamma))
+        v_term = np.sqrt(
+            1
+            - cos_alpha * cos_alpha
+            - cos_beta * cos_beta
+            - cos_gamma * cos_gamma
+            + 2 * cos_alpha * cos_beta * cos_gamma
+        )
+
+        sin_gamma = np.sin(np.deg2rad(gamma))
+
+        a_vec = np.asarray([a, 0, 0])
+        b_vec = np.asarray([b * cos_gamma, b * sin_gamma, 0])
+        c_vec = np.asarray([c * cos_beta, c * (cos_alpha - cos_gamma * cos_beta) / sin_gamma, c * v_term / sin_gamma])
+
+        return Lattice(a_vec, b_vec, c_vec)
+
+    def __scalars_are_valid(a: float, b: float, c: float, alpha: float, beta: float, gamma: float) -> bool:
+        return a > 0 and b > 0 and c > 0 and alpha > 0 and beta > 0 and gamma > 0
+
+    def constructFromVectors(a_vec, b_vec, c_vec) -> Lattice:
+        return Lattice(a_vec, b_vec, c_vec)
--- tests/test_lattice.py
+++ tests/test_lattice.py
@@ -2,7 +2,7 @@ import pytest
 import numpy as np
 import numpy.testing as nptest
 
-from lattice import Lattice
+from lattice import LatticeBuilder
 
 
 def assert_dotprod(left, right, angle):
@@ -18,7 +18,7 @@ def test_vector_cubic():
     a, b, c = 1, 1, 1
     alpha, beta, gamma = 90, 90, 90
 
-    lattice = Lattice(a, b, c, alpha, beta, gamma)
+    lattice = LatticeBuilder.constructFromScalars(a, b, c, alpha, beta, gamma)
 
     nptest.assert_allclose(lattice.a_vec, [a, 0, 0], atol=0.00001)
     nptest.assert_allclose(lattice.b_vec, [0, b, 0], atol=0.00001)
@@ -33,7 +33,7 @@ def test_vector_hexagonal():
     a, b, c = 1, 1, 3
     alpha, beta, gamma = 90, 90, 120
 
-    lattice = Lattice(a, b, c, alpha, beta, gamma)
+    lattice = LatticeBuilder.constructFromScalars(a, b, c, alpha, beta, gamma)
 
     nptest.assert_allclose(lattice.a_vec, [a, 0, 0], atol=0.00001)
     nptest.assert_allclose(lattice.b_vec[2], 0, atol=0.00001)
@@ -49,7 +49,7 @@ def test_vector_monoclinic():
     a, b, c = 1, 2, 3
     alpha, beta, gamma = 60, 70, 80
 
-    lattice = Lattice(a, b, c, alpha, beta, gamma)
+    lattice = LatticeBuilder.constructFromScalars(a, b, c, alpha, beta, gamma)
 
     nptest.assert_allclose(lattice.a_vec, [a, 0, 0], atol=0.00001)
     nptest.assert_allclose(np.dot(lattice.b_vec, lattice.b_vec), b * b, atol=0.00001)
@@ -60,24 +60,22 @@ def test_vector_monoclinic():
     assert_dotprod(lattice.b_vec, lattice.c_vec, alpha)
 
 
-def test_reciprocal():
-    a, b, c = 1, 1, 1
-    alpha, beta, gamma = 90, 90, 90
+@pytest.mark.parametrize(
+    "lattice_constants",
+    [(1, 1, 1, 90, 90, 90), (1, 1, 3, 90, 90, 120), (1, 2, 3, 60, 70, 80)],
+    ids=("cubic", "hexagonal", "triclinic"),
+)
+def test_reciprocal_roundtrip(lattice_constants):
+    a, b, c = lattice_constants[:3]
+    alpha, beta, gamma = lattice_constants[3:]
 
-    lattice = Lattice(a, b, c, alpha, beta, gamma)
+    lattice = LatticeBuilder.constructFromScalars(a, b, c, alpha, beta, gamma)
+
     reciprocal = lattice.reciprocal()
 
     backtoorig = reciprocal.reciprocal()
 
     lattice.assert_allclose(backtoorig)
-
-    # found = calc_reciprocal(*recip)
-    # assert found[0] == a
-    # assert found[1] == b
-    # assert found[2] == c
-    # assert found[3] == alpha
-    # assert found[4] == beta
-    # assert found[5] == gamma
 
 
 if __name__ == "__main__":
