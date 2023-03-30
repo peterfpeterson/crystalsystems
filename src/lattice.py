ac6c54d244d4f118190586f5352d50dccf8509b3
Pete Peterson <petersonpf@ornl.gov>
2023-03-30 10:50:11 -0400

    Move function body into class method

--- src/lattice.py
+++ src/lattice.py
@@ -6,7 +6,24 @@ class Lattice:
         self.set_scalars(a, b, c, alpha, beta, gamma)
 
     def set_scalars(self, a, b, c, alpha, beta, gamma):
-        self.a_vec, self.b_vec, self.c_vec = to_vector(a, b, c, alpha, beta, gamma)
+        cos_alpha = np.cos(np.deg2rad(alpha))
+        cos_beta = np.cos(np.deg2rad(beta))
+        cos_gamma = np.cos(np.deg2rad(gamma))
+        v_term = np.sqrt(
+            1
+            - cos_alpha * cos_alpha
+            - cos_beta * cos_beta
+            - cos_gamma * cos_gamma
+            + 2 * cos_alpha * cos_beta * cos_gamma
+        )
+
+        sin_gamma = np.sin(np.deg2rad(gamma))
+
+        self.a_vec = np.asarray([a, 0, 0])
+        self.b_vec = np.asarray([b * cos_gamma, b * sin_gamma, 0])
+        self.c_vec = np.asarray(
+            [c * cos_beta, c * (cos_alpha - cos_gamma * cos_beta) / sin_gamma, c * v_term / sin_gamma]
+        )
 
     @property
     def volume(self) -> float:
@@ -28,20 +45,3 @@ class Lattice:
         np.testing.assert_allclose(self.a_vec, other.a_vec, atol=atol)
         np.testing.assert_allclose(self.b_vec, other.b_vec, atol=atol)
         np.testing.assert_allclose(self.c_vec, other.c_vec, atol=atol)
-
-
-def to_vector(a, b, c, alpha, beta, gamma):
-    cos_alpha = np.cos(np.deg2rad(alpha))
-    cos_beta = np.cos(np.deg2rad(beta))
-    cos_gamma = np.cos(np.deg2rad(gamma))
-    v_term = np.sqrt(
-        1 - cos_alpha * cos_alpha - cos_beta * cos_beta - cos_gamma * cos_gamma + 2 * cos_alpha * cos_beta * cos_gamma
-    )
-
-    sin_gamma = np.sin(np.deg2rad(gamma))
-
-    a_vec = np.asarray([a, 0, 0])
-    b_vec = np.asarray([b * cos_gamma, b * sin_gamma, 0])
-    c_vec = np.asarray([c * cos_beta, c * (cos_alpha - cos_gamma * cos_beta) / sin_gamma, c * v_term / sin_gamma])
-
-    return a_vec, b_vec, c_vec
